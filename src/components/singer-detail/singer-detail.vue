5e11037b46160c26ce43be6edac404c5851171e6
liuyilan <2995293916@qq.com>
2019-01-19 23:43:50 +0800

    finish chapter7

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,36 @@
+<template>
+<transition name="slide">
+  <div class="singer-detail"></div>
+</transition>
+</template>
+<script>
+  import { mapGetters} from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created() {
+      console.log(this.singer)
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .singer-detail
+    position: fixed
+    z-index:100
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: $color-background
+
+  .slide-enter-active,.slide-leave-active
+    transition: all .3s
+  .slide-enter,.slide-leave-to
+    transform: translate3d(100%,0,0)
+</style>
+
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as actions from './actions'
+import * as getters from './getters'
+import state from './state'
+import mutations from './mutations'
+import createLogger from 'vuex/dist/logger'
+
+Vue.use(Vuex)
+// 开发环境使用
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  state,
+  mutations,
+  debug,
+  strict: debug,
+  plugins: debug ? [createLogger()] : ''
+})
