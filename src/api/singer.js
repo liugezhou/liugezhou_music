2e061b0e1d776f4a6706122ef10eb20bc3baa8db
liugezhou <825386039@qq.com>
2019-01-20 22:45:32 +0800

    finish chapter7

--- src/api/singer.js
+++ src/api/singer.js
@@ -17,3 +17,19 @@ export function getSingerList() {
 
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    singermid: singerId,
+    order: 'listen',
+    begin: 0,
+    num: 100,
+    songstatus: 1
+  })
+  return jsonp(url, data, options)
+}
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,33 +1,66 @@
 <template>
 <transition name="slide">
-  <div class="singer-detail"></div>
+  <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
 </transition>
 </template>
 <script>
   import { mapGetters} from 'vuex'
+  import { getSingerDetail} from 'api/singer'
+  import { ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
+
   export default {
+    data(){
+      return {
+        songs:[]
+      }
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage(){
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created() {
-      console.log(this.singer)
+      this._getDetail()
+
+    },
+    methods: {
+      _getDetail() {
+        if(!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if(res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+          }
+        })
+      },
+      _normalizeSongs(list){
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} =item
+          if(musicData.songid && musicData.albummid){
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 <style lang="stylus" scoped>
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index:100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-
   .slide-enter-active,.slide-leave-active
     transition: all .3s
   .slide-enter,.slide-leave-to
