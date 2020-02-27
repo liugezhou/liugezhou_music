5a3d998157918969127c90e3e91e94646c417a47
liumingzhou <18231133236@163.com>
2020-02-27 11:15:48 +0800

    update script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'VueMusic',
-    script: 'pro.server.js',
+    script: 'prod.server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
